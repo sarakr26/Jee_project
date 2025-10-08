c688c37c7ad9914b507d5203009289455717a8e6
Sara krichi <krichi.2003.sara@gmail.com>
2025-10-08 15:30:59 +0100

    Page Liste des événements + formulaire création événement

---
+++ src/main/java/com/projet/jee/dao/DBConnection.java
@@ -0,0 +1,23 @@
+package com.projet.jee.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/chess_club_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = ""; // change if you have a password
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
